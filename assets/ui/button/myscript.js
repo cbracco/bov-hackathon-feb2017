07fbba80eee38b310b960917eda93291998b8ddf
Chris Bracco <chris@cbracco.me>
2017-02-26 12:20:30 -0500

    Update button UI, add button UI to showcase

--- assets/ui/button/myscript.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(".btns-markup").click(function() {
-        if ($( this ).hasClass("rectangle")) {
-            $("#basicCode").toggleClass("hide");
-        } else if ($( this ).hasClass("round")) {
-            $("#roundedCode").toggleClass("hide");
-        }
-            })
-})
