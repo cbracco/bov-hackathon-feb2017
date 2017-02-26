ea44fac93b8a3e4b8abdcd4f73cb67a495a4efd2
Chris Bracco <chris@cbracco.me>
2017-02-26 11:20:52 -0500

    Add position sticky polyfill, scrollspy

---
+++ assets/ui/scrollspy/scrollspy.js
@@ -0,0 +1,36 @@
+/**
+ * Scrollspy
+ * @author Chris Bracco
+ */
+var scrollSpy = (function () {
+  // STRICT MODE
+  'use strict';
+
+  //
+  // PUBLIC METHODS
+  //
+  function init () {
+    var sections = {};
+    var section = document.querySelectorAll('.js-scrollspy-section');
+    var i = 0;
+
+    Array.prototype.forEach.call(section, function (event) {
+      sections[event.id] = event.offsetTop;
+    });
+
+    document.addEventListener('scroll', function (event) {
+      var scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
+
+      for (i in sections) {
+        if (sections[i] <= scrollPos) {
+          document.querySelector('.js-scrollspy-link.is-current').classList.remove('is-current');
+          document.querySelector('.js-scrollspy-link[href*=' + i + ']').classList.add('is-current');
+        }
+      }
+    }, false);
+  }
+
+  return {
+    init: init
+  }
+})();
